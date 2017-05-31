584981869a2c10295c817d0b360c4d8644bf1741
Alena Golovina and Saul Artiga <saul@>
2017-05-31 10:20:13 -0700

    complete first draft of flashcards

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  $("#flashcard1").click(function() {
+    $(".hidden-definition-1").toggle();
+    $("#showing-definition-javascript").toggle();
+  });
+  $("#flashcard2").click(function() {
+    $(".hidden-definition").toggle();
+    $("#showing-definition-operators").toggle();
+  });
+  $("#flashcard3").click(function() {
+    $(".hidden-definition").toggle();
+    $("#showing-definition-variables").toggle();
+  });
+  $("#flashcard4").click(function() {
+    $(".hidden-definition").toggle();
+    $("#showing-definition-functions").toggle();
+  });
+  $("#flashcard5").click(function() {
+    $(".hidden-definition").toggle();
+    $("#showing-definition-methods").toggle();
+  });
+  $("#flashcard6").click(function() {
+    $(".hidden-definition").toggle();
+    $("#showing-definition-arguments").toggle();
+  });
+  $("#flashcard7").click(function() {
+    $(".hidden-definition").toggle();
+    $("#showing-definition-parameters").toggle();
+  });
+  $("#flashcard8").click(function() {
+    $(".hidden-definition").toggle();
+    $("#showing-definition-strings").toggle();
+  });
+});
